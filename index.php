d18286fdcffa94ab27317f4b7448285b7d236f32
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-01-23 15:15:02 +0100

    foreach con echo che stampa i dati in lista ora aggiungo la tabella da bootstrap

---
+++ index.php
@@ -0,0 +1,102 @@
+<!-- CONSEGNA :
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+
+Iniziate in modo graduale.
+Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella. -->
+
+<?php
+$hotels = [
+
+	[
+		'name' => 'Hotel Belvedere',
+		'description' => 'Hotel Belvedere Descrizione',
+		'parking' => true,
+		'vote' => 4,
+		'distance_to_center' => 10.4
+	],
+	[
+		'name' => 'Hotel Futuro',
+		'description' => 'Hotel Futuro Descrizione',
+		'parking' => true,
+		'vote' => 2,
+		'distance_to_center' => 2
+	],
+	[
+		'name' => 'Hotel Rivamare',
+		'description' => 'Hotel Rivamare Descrizione',
+		'parking' => false,
+		'vote' => 1,
+		'distance_to_center' => 1
+	],
+	[
+		'name' => 'Hotel Bellavista',
+		'description' => 'Hotel Bellavista Descrizione',
+		'parking' => false,
+		'vote' => 5,
+		'distance_to_center' => 5.5
+	],
+	[
+		'name' => 'Hotel Milano',
+		'description' => 'Hotel Milano Descrizione',
+		'parking' => true,
+		'vote' => 2,
+		'distance_to_center' => 50
+	],
+
+];
+
+// stampo tutto a schermo 
+foreach($hotels as $hotel){
+    $name = $hotel["name"];
+    $description = $hotel["description"];
+    $parking = $hotel["parking"];
+    $vote = $hotel["vote"];
+    $distance_to_center = $hotel["distance_to_center"];
+
+    // echo che stampa tutti i dati in lista 
+
+    // echo "Nome Hotel :" . $name . "<br>" . "Descrizione :" . $description . "<br>" . "Parcheggio" . $parking . "<br>" . "voto : " . $vote . "<br>" . "distanza dal centro :" . $distance_to_center; 
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="import" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css.html">
+    <title>Document</title>
+</head>
+<body>
+<table class="table table-striped">
+<thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    
+</body>
+</html>
